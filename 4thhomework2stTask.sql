01cd8654e89f16f629d6b4f45400d58d8cddddf1
Nick <sshedl22@gmail.com>
2020-04-12 14:24:44 +0300

    Typos

--- 4thhomework2stTask.sql
+++ 4thhomework2stTask.sql
@@ -16,9 +16,9 @@ insert into users(`name`, `birthday`) values
 ("Rita", "1970-01-01" + INTERVAL FLOOR(RAND() * 365 * 30) DAY),
 ("Nastya", "1970-01-01" + INTERVAL FLOOR(RAND() * 365 * 30) DAY),
 ("Vova", "1970-01-01" + INTERVAL FLOOR(RAND() * 365 * 30) DAY);
--- вывод исходной таблицы со столбцом дней недели когда они родились 
+-- вывод исходной таблицы + столбец с № дня недели когда было др в год рождения  
 SELECT name,birthday ,DAYOFWEEK(birthday) FROM users; 
--- вывод таблицы со столбцом дней недели когда когда у них др в этом году
+-- вывод таблицы + столбец с № дня недели когда было др в этот год.
 SELECT name, DAYOFWEEK(REPLACE(birthday, YEAR(birthday), YEAR(NOW()))) FROM users;
 -- количество дней рождения, которые приходятся на каждый из дней недели в этом году
 SELECT COUNT(*), DAYOFWEEK(REPLACE(birthday, YEAR(birthday), YEAR(NOW()))) as day_of_week FROM users group by day_of_week;
